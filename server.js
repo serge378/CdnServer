b5a8c4cf240cd5e6c4e71a6f23fd6500217bdeff
serge bruno <sergebruno378@gmail.com>
2020-08-29 19:42:01 +0000

    Ajout page 404

--- server.js
+++ server.js
@@ -3,4 +3,11 @@ const app = express();
 
 app.use("/static", express.static("static"));
 
+app.set("views", __dirname + "/views");
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render("404.ejs");
+});
+
 app.listen(8080);
